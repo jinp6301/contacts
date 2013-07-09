ab562cb7058d196059b53e450072a20c281983ad
Anthony Mangano and Jin Park <Anthony+Jin@appacademy.io>
2013-07-09 09:01:35 -0400

    Initial commit

---
+++ contacts/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacts::Application.config.secret_token = 'd62818707bc1d3872988f5e21831c41b295c64d1ae40f95707136c0a0666a3dad7ddacc6fa29d7c04dc032d59ad17f39b92ffd33ae12b834f208f1e9a796bb35'
